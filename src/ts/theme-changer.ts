57ad194a68bbb406de02d4154b7de8ba712ec258
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-08-30 16:51:32 +0200

    Dark theme future

--- src/ts/theme-changer.ts
+++ src/ts/theme-changer.ts
@@ -1,6 +1,10 @@
 // Theme changer controller
 const themeChanger: HTMLButtonElement = document.querySelector(".todo-app__theme-changer") as HTMLButtonElement;
+const todoApp: HTMLElement = document.querySelector(".todo-app") as HTMLElement;
+const body: HTMLElement = document.querySelector(".body") as HTMLElement;
 
 themeChanger.addEventListener("click", function (): void {
     this.classList.toggle("todo-app__theme-changer--active");
+    todoApp.classList.toggle("todo-app--dark");
+    body.classList.toggle("body--dark");
 });
