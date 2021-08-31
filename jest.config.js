fc886e0e1a3c2e594a6f755dd7458125ff74970c
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-09-01 01:58:43 +0200

    Create jest config file with basic config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    transform: {
+        "^.+\\.[t|j]sx?$": "babel-jest"
+    },
+    verbose: true,
+    testEnvironment: "jsdom",
+    moduleNameMapper: {
+        '\\.svg$': '<rootDir>/src/__mocks__/svgrMock.ts',
+    },
+    // collectCoverage: true,
+    coverageDirectory: "<rootDir>/src/__tests__/coverage"
+
+}
